e884caaa1ce0a2455add754bc7b9ce634ae35988
visenger <visenger@gmail.com>
2014-12-27 22:37:57 +0100

    Initial commit

---
+++ src/test/java/HelloJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by visenger on 27/12/14.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ src/test/scala/HelloScala.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by visenger on 27/12/14.
+ */
+object HelloScala  {
+
+  def main(args: Array[String]) {
+    println("hello scala")
+  }
+
+}
